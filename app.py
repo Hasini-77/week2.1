3c19a732d9d1c81bbbea23a71cc249eb41719a21
Hasini-77 <23251a1226@gmail.com>
2026-09-17 21:32:06 +0530

    final changes

--- app.py
+++ app.py
@@ -1,2 +1,26 @@
-print("Sample program for git and git commands")
 
+"""from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+app.run(debug=True)"""
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form['name']
+    email = request.form['email']
+    course = request.form['course']
+    return render_template('success.html', name=name, email=email, course=course)
+
+if __name__ == '__main__':
+    app.run(debug=True)
